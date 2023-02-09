a349df4b824d6c6f591f25f87745623888972f43
Preston Moore <caroham29@gmail.com>
2023-02-08 21:51:46 -0800

    static data refactor

--- components/homepage/homepage.js
+++ components/homepage/homepage.js
@@ -16,6 +16,12 @@ import { BiLocationPlus } from "react-icons/bi";
 import { categories } from "../../services/static-data";
 import { AppTheme } from "../../pages/_app";
 import { CardPopup } from "./card-popup/card-popup";
+import {
+	businessArr,
+	restArr,
+	housingArr,
+	politicsArr,
+} from "../../public/utils/static-data";
 
 // Hooks useEffect, useConext, useState, useRef, useLayoutEffect, useTransition
 const Homepage = () => {
@@ -23,148 +29,59 @@ const Homepage = () => {
 	const dataRef = useRef();
 	const arr = [];
 	let start = false;
+
+	// Item control by 4 categories --->
 	const [showFoodItem, setShowFoodItem] = useState([
 		false,
 		false,
 		false,
 		false,
 	]);
+
+	const [showRealEstateItem, setShowRealEstateItem] = useState([
+		false,
+		false,
+		false,
+		false,
+	]);
+
+	const [showBusinessItem, setShowBusinessItem] = useState([
+		false,
+		false,
+		false,
+		false,
+	]);
+
+	const [showPoliticsItem, setShowPoliticsItem] = useState([
+		false,
+		false,
+		false,
+		false,
+	]);
+	// <--- End of item control
+
 	const [businessCtrl, setBusinessCtrl] = useState([
 		false,
 		false,
 		false,
 		false,
 	]);
+
 	const [foodCtrl, setfoodCtrl] = useState([false, false, false, false]);
+
 	const [housingCtrl, setHousingCtrl] = useState([
 		false,
 		false,
 		false,
 		false,
 	]);
+
 	const [politicsCtrl, setPoliticsCtrl] = useState([
 		false,
 		false,
 		false,
 		false,
 	]);
-	const dataArr = [3, 4, 5, 3, 3, 3, 2, 1, 7, 3, 5, 9, 1, 9, 9, 9, 9, 9, 9]; // test [9,9,9]
-	const businessArr = [
-		{
-			src: "/tutor.jpeg",
-			user: "Nancy Cooks",
-			title: "Private tutoring with Nancy",
-			desc: "Give a child a headstart with tutoring. I'm a teacher with 5 years teaching experience",
-		},
-		{
-			src: "/creditOption.jpeg",
-			user: "Credit Master Sean",
-			title: "Have an 800 Credit Score Yet",
-			desc: "We can get you there! 5 star credit repair at Valley Wide Credit Repair",
-		},
-		{
-			src: "/stylist.jpg",
-			user: "Kesha Kay",
-			title: "Is Your Hair Ready For The Big Event",
-			desc: "Licensed stylist specializing in braids, and dreadlocks",
-		},
-		{
-			src: "/business_4.jpg",
-			user: "Don McStashinbuggle",
-			title: "Guaranteed Issue Whole Life Policy Up To 1 Million",
-			desc: "Licensed insurance agent to secure generational wealth for your family",
-		},
-	];
-	const restArr = [
-		{
-			src: "/rest_1.jpg",
-			user: "Honey Tea LLC",
-			title: "Have You Heard of Just Add Honey Tea Company",
-			desc: "Blended tea's for everyone! Sweetened and served just for you",
-		},
-		{
-			src: "/rest_2.jpg",
-			user: "Hammin's All Star BBQ",
-			title: "Lip Smackin Hammin's BBQ Will Leave You Speachless",
-			desc: "Award winning BBQ for the entire family. Pull up and see what everyone's talking about",
-		},
-		{
-			src: "/rest_3.jpg",
-			user: "Wendy's Kitchen",
-			title: "Wendy's Southern Kitchen Is The Best in the Midwest",
-			desc: "Traditional Souther Cousine with a midwest twist",
-		},
-		{
-			src: "/rest_4.jpg",
-			user: "Carl Kitchens",
-			title: "Have You Tasted What The Hype is About At Cartel's Kitchen",
-			desc: "Our signature fried wings and thigs are quickly becoming legendary",
-		},
-	];
-	const housingArr = [
-		{
-			src: "/housing_1.jpg",
-			user: "Paramount Properties LLC",
-			title: "Modern 1 Bedroom Studio in Heart of Downtown",
-			desc: "In the heart of the much desired downtown district",
-		},
-		{
-			src: "/housing_2.jpg",
-			user: "Yannique Golden",
-			title: "3 Bedroom 2 Bath Home For Rent in Central Fresno",
-			desc: "Spacious home recently remodeled in Central Unified school district",
-		},
-		{
-			src: "/housing_3.jpg",
-			user: "AJ For The Win",
-			title: "2 Bedroom 1.5 Bath Apartment For Rent in Anticoch",
-			desc: "Two story apartment with spacious patio and attached garage",
-		},
-		{
-			src: "/housing_4.jpg",
-			user: "New Beginnings Properties LLC",
-			title: "4 Bedroom 2 Bath Home for Sale in Clovis",
-			desc: "Over 2500 square feet of modern living with a hint of country charm",
-		},
-	];
-	const politicsArr = [
-		{
-			src: "/politics_1.jpg",
-			user: "Johnny the Activist",
-			title: "Gun Rights Activist Says Black Guns Matter",
-			desc: "Private tutoring with Nancy",
-		},
-		{
-			src: "/politics_2.jpg",
-			user: "Highlife Movements",
-			title: "Why Reparations 2023 May Be Reality For Californian's",
-			desc: "5 star credit repair at Valley Wide Credit Repair",
-		},
-		{
-			src: "/politics_3.jpg",
-			user: "Tremale Jacobs",
-			title: "Why Mattlock Belives He Has The Winning Ticket",
-			desc: "We belive in results and let our actions do the talking",
-		},
-		{
-			src: "/politics_4.jpg",
-			user: "Black Insider",
-			title: "Why These Two California Politians Are Causing a Stir This Election Season",
-			desc: "Looking for solutions in 2023? Look no further than these two local activist running for office",
-		},
-	];
-	for (let i = dataArr.length - 1; i >= 0; i--) {
-		if (dataArr[i] >= 9) {
-			start = true;
-			dataArr[i] = 0;
-			dataArr[i - 1]++;
-		} else {
-			if (start) {
-				break;
-			}
-		}
-	}
-
 	const scrollDown = () => {
 		dataRef.current.scrollIntoView({ behavior: "smooth" });
 	};
@@ -180,13 +97,8 @@ const Homepage = () => {
 	const passedIn = useContext(AppTheme);
 
 	useEffect(() => {
-		console.log(inputRef.current, " Hello Sammy ", passedIn);
 		inputRef.current.focus();
 		var item = document.getElementById("inputItemSearch");
-		// setTimeout(() => {
-		// 	item.focus();
-		// }, 250);
-		console.log(" Hello ", categories);
 	}, []);
 
 	const navigateAway = (url) => {
@@ -341,13 +253,30 @@ const Homepage = () => {
 										>
 											{it.title}
 										</div>
+										{!!showBusinessItem[ind] && (
+											<CardPopup />
+										)}
 										<div
 											className={
 												styles.titleContainerOption
 											}
 										>
-											{!!businessCtrl[ind] && (
-												<i className="bi bi-three-dots-vertical"></i>
+											{(!!businessCtrl[ind] ||
+												!!showBusinessItem[ind]) && (
+												<i
+													onClick={() =>
+														setShowBusinessItem(
+															showBusinessItem.map(
+																(item, index) =>
+																	index ===
+																	ind
+																		? !item
+																		: false
+															)
+														)
+													}
+													className="bi bi-three-dots-vertical"
+												></i>
 											)}
 										</div>
 									</div>
@@ -470,13 +399,30 @@ const Homepage = () => {
 										>
 											{it.title}
 										</div>
+										{!!showRealEstateItem[ind] && (
+											<CardPopup />
+										)}
 										<div
 											className={
 												styles.titleContainerOption
 											}
 										>
-											{!!housingCtrl[ind] && (
-												<i className="bi bi-three-dots-vertical"></i>
+											{(!!housingCtrl[ind] ||
+												!!showRealEstateItem[ind]) && (
+												<i
+													onClick={() =>
+														setShowRealEstateItem(
+															showRealEstateItem.map(
+																(item, index) =>
+																	index ===
+																	ind
+																		? !item
+																		: false
+															)
+														)
+													}
+													className="bi bi-three-dots-vertical"
+												></i>
 											)}
 										</div>
 									</div>
@@ -529,13 +475,30 @@ const Homepage = () => {
 										>
 											{it.title}
 										</div>
+										{!!showPoliticsItem[ind] && (
+											<CardPopup />
+										)}
 										<div
 											className={
 												styles.titleContainerOption
 											}
 										>
-											{!!politicsCtrl[ind] && (
-												<i className="bi bi-three-dots-vertical"></i>
+											{(!!politicsCtrl[ind] ||
+												!!showPoliticsItem[ind]) && (
+												<i
+													onClick={() =>
+														setShowPoliticsItem(
+															showPoliticsItem.map(
+																(item, index) =>
+																	index ===
+																	ind
+																		? !item
+																		: false
+															)
+														)
+													}
+													className="bi bi-three-dots-vertical"
+												></i>
 											)}
 										</div>
 									</div>
