ca5a6f2d9aed6e78f0e3c3393dc795ce1f13779a
Preston Moore <caroham29@gmail.com>
2023-02-20 11:36:50 -0800

    inquiryForm

--- components/single-listing/single-listing.js
+++ components/single-listing/single-listing.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./single-listing.module.css";
@@ -11,6 +11,11 @@ import StarRatings from "react-star-ratings";
 import decodeHTMLEntities from "decode-html";
 import { htmlEncode } from "htmlencode";
 import ReactHtmlParser from "react-html-parser";
+import { useFormikContext, Formik, Form, Field } from "formik";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import ReCAPTCHA from "react-google-recaptcha";
 import {
 	FacebookIcon,
 	FacebookMessengerIcon,
@@ -38,9 +43,10 @@ import {
 */
 
 const SingleListings = () => {
-	const longSTRING = htmlEncode(`<div className={styles.reviewContainer}>
-		<p className={styles.reviewContainerDivAlt}>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, <a href="https://www.youtube.com/@TheAngryman"  target="_blank">sed do eiusmod tempor</a> incididunt ut labore et dolore magna aliqua. Nec ullamcorper sit amet risus nullam eget felis. Sociis natoque penatibus et magnis. Elit ut aliquam purus sit amet. Nibh tellus molestie nunc non blandit massa enim nec. Ut tortor pretium viverra suspendisse potenti nullam ac.
+	const captchaRef = useRef(null);
+	const longSTRING = htmlEncode(`<div>
+		<p >
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, <a href="https://www.youtube.com/@TheAngryman" target="_blank">sed do eiusmod tempor</a> incididunt ut labore et dolore magna aliqua. Nec ullamcorper sit amet risus nullam eget felis. Sociis natoque penatibus et magnis. Elit ut aliquam purus sit amet. Nibh tellus molestie nunc non blandit massa enim nec. Ut tortor pretium viverra suspendisse potenti nullam ac.
 		</p>
 		<p>Urna condimentum mattis pellentesque id. Mauris pellentesque pulvinar pellentesque habitant. Eu volutpat odio facilisis mauris sit amet massa. Rutrum quisque non tellus orci ac auctor augue. Ipsum dolor sit amet consectetur. </p>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec ullamcorper sit amet risus nullam eget felis. Sociis natoque penatibus et magnis. Elit ut aliquam purus sit amet. Nibh tellus molestie nunc non blandit massa enim nec. Ut tortor pretium viverra suspendisse potenti nullam ac.</p>
@@ -52,6 +58,7 @@ const SingleListings = () => {
 	const { lid } = router.query;
 	const [listing, setListing] = useState({});
 	const [article, setArticle] = useState(null);
+	const [showCapErr, setShowCapErr] = useState(false);
 
 	useEffect(() => {
 		if (!lid) {
@@ -104,15 +111,170 @@ const SingleListings = () => {
 					</div>
 				</div>
 				<div className={styles.postBody}>
-					<div className={styles.postBodyLeft}>
-						<div className={styles.imgContainer}>
-							<img src={listing.url} className={styles.imgMain} />
+					<div className={styles.imgContainer}>
+						<img src={listing.url} className={styles.imgMain} />
+					</div>
+					<div className={styles.flexMe}>
+						<div className={styles.postBodyLeft}>
+							<div className={styles.innerBodyContainer}>
+								<div>{ReactHtmlParser(article)}</div>
+							</div>
 						</div>
-						<div className={styles.innerBodyContainer}>
-							<div>{ReactHtmlParser(article)}</div>
+						<div className={styles.postBodyRight}>
+							<div className={styles.postBodyRightContainer}>
+								Contact {listing.username}
+							</div>
+							<div className={styles.postBodyRightContainerBody}>
+								<div>
+									<i className="bi bi-telephone"></i>
+									<span>(209)421-2109</span>
+								</div>
+								<div>
+									<i className="bi bi-globe"></i>
+									<span>
+										<a
+											href="https://www.youtube.com/@TheAngryman"
+											target="_blank"
+										>
+											https://www.youtube.com/@TheAngryman
+										</a>
+									</span>
+								</div>
+								<div></div>
+							</div>
+
+							<div
+								className={`${styles.postBodyRightContainer} mt-5`}
+							>
+								Send Inquiry
+							</div>
+							<div className={styles.postBodyRightContainerBody}>
+								<Formik
+									initialValues={{
+										name: "",
+										email: "",
+										phone: "",
+										message: "",
+									}}
+								>
+									{({
+										setFieldValue,
+										values,
+										handleChange,
+										touched,
+										errors,
+									}) => (
+										<Form>
+											<TextField
+												fullWidth
+												id="name"
+												name="name"
+												required
+												variant="outlined"
+												label="Name"
+												className={`my-2 ${styles.customBorders}`}
+												value={values.name}
+												onChange={handleChange}
+												error={
+													touched.name &&
+													Boolean(errors.name)
+												}
+												helperText={
+													touched.name && errors.name
+												}
+											/>
+											<TextField
+												className={`my-2 ${styles.customBorders}`}
+												fullWidth
+												id="email"
+												required
+												variant="outlined"
+												name="email"
+												label="Email"
+												value={values.email}
+												onChange={handleChange}
+												error={
+													touched.email &&
+													Boolean(errors.email)
+												}
+												helperText={
+													touched.email &&
+													errors.email
+												}
+											/>
+											<TextField
+												className={`my-2 ${styles.customBorders}`}
+												fullWidth
+												id="phone"
+												InputProps={{
+													inputProps: {
+														style: {
+															bordercolor:
+																"#008080",
+															outlinecolor:
+																"#008080",
+														},
+													},
+												}}
+												required
+												variant="outlined"
+												name="Phone"
+												label="Phone"
+												value={values.phone}
+												onChange={handleChange}
+												error={
+													touched.phone &&
+													Boolean(errors.phone)
+												}
+												helperText={
+													touched.phone &&
+													errors.phone
+												}
+											/>
+											<TextareaAutosize
+												className={`my-2 ${styles.textareaCustom} ${styles.customBorders}`}
+												minRows={5}
+												id="message"
+												required
+												variant="outlined"
+												name="message"
+												label="message"
+												placeholder="Message"
+												value={values.message}
+												onChange={handleChange}
+												error={
+													touched.message &&
+													Boolean(errors.message)
+												}
+											/>
+											<ReCAPTCHA
+												className={`mt-3`}
+												onChange={(val) =>
+													setShowCapErr(!val)
+												}
+												sitekey="6LdKXJEkAAAAAI3au3h6Lu1OSlhK_pVt-eVI_xEE"
+												ref={captchaRef}
+											/>
+											{showCapErr && (
+												<small>
+													CAPTCHA response required.
+												</small>
+											)}
+											<Button
+												className={`mt-2 mb-3 w-100 ${styles.signInBtn}`}
+												color="primary"
+												variant="contained"
+												fullWidth
+												type="submit"
+											>
+												submit
+											</Button>
+										</Form>
+									)}
+								</Formik>
+							</div>
 						</div>
 					</div>
-					<div className={styles.postBodyRight}></div>
 				</div>
 			</div>
 		</>
