9d46dd6a57debf516ee3c7f13aac4314b24feac4
Preston Moore <caroham29@gmail.com>
2023-06-12 15:22:45 -0700

    preDeploy

--- afro-listings.server.js
+++ afro-listings.server.js
@@ -12,7 +12,7 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 var pg = require("pg");
-pg.defaults.ssl = true;
+// pg.defaults.ssl = true;
 var connectionString = process.env.CONNECTION_STRING;
 const client = new pg.Client({
   connectionString: connectionString,
