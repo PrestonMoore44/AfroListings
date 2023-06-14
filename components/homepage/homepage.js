ae49f371f98d4e29af53ae040bf0d1ec050fd71b
Preston Moore <caroham29@gmail.com>
2023-06-13 18:42:58 -0700

    pm(feat)scrollTransition

--- components/homepage/homepage.js
+++ components/homepage/homepage.js
@@ -20,6 +20,7 @@ import Cover from "../cover/cover";
 import { getListings } from "../../lib/services/listings-service";
 import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.core.globals("ScrollTrigger", ScrollTrigger);
 gsap.registerPlugin(ScrollTrigger);
 
 const Homepage = () => {
